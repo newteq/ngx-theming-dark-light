24326884bf15477aed73728a098ed2b589a832cc
Newt Fourie <NewteqDeveloper@users.noreply.github.com>
2019-05-21 12:57:49 +0200

    Add new powershell to help deploy to github pages

---
+++ ngx-theming/github-pages/deploy-to-pages.ps1
@@ -0,0 +1,2 @@
+ng build --prod --base-href "https://newteqdeveloper.github.io/ngx-theming-dark-light" --deploy-url "https://newteqdeveloper.github.io/ngx-theming-dark-light/"
+ngh -d dist/ngx-theming --message="Updating demo website with automated build"
